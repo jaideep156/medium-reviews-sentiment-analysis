4938297dbe9c8db1f2e22bb62f50ed3583c20679
Jaideep Prashar <jaideep156@gmail.com>
2024-03-03 12:39:02 +0530

    app modifications

--- predict_page.py
+++ predict_page.py
@@ -12,8 +12,12 @@ vectorizer = data["vectorizer"]
 
 def show_predict_page():
     st.title("Medium App Reviews Sentiment Analysis")
-    
-    text = st.text_input("Enter the review and press the button below")
+
+    st.subheader("This is a sentiment analyzer of reviews of the [Medium](https://play.google.com/store/apps/details?id=com.medium.reader) app from Google Play Store")
+
+    st.markdown("The dataset along with data dictionary can be found on [Kaggle](https://www.kaggle.com/datasets/raqhea/medium-app-reviews-from-google-play-store/).")
+
+    text = st.text_input("Enter your review and press submit")
     ok = st.button("Submit")
 
     if ok:
@@ -22,3 +26,5 @@ def show_predict_page():
         predicted_sentiments = label_encoder.inverse_transform(predictions)
 
         st.write('The predicted sentiment of the above review is:',predicted_sentiments[0].lower())
+
+    st.write("##### You can find the full codebase & project specifics on my [GitHub](https://github.com/jaideep156/medium-reviews-sentiment-analysis).")
